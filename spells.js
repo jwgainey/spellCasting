98ce7f8919ff988e759167091883df843c0d7b6c
Joel Gainey <jwgainey@hotmail.com>
2020-01-06 23:23:51 -0500

    spell casting, first stage, var and fireball added

---
+++ spells.js
@@ -0,0 +1,15 @@
+var d4 = Math.floor(Math.random()*4) + 1;
+var d6 = Math.floor(Math.random()*6) + 1;
+var d8 = Math.floor(Math.random()*8) + 1;
+var d10 = Math.floor(Math.random()*10) + 1;
+var spellCast = prompt("What spell do you cast?");
+
+
+if ( spellCast === 'fireball' )
+var fireball, dmg = 0;
+   for (var i = 0; i < 8; i += 1) {
+      fireball = d6;
+      dmg += fireball;  
+   }
+
+   alert(dmg);
