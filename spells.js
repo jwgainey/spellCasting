3c246d0683b72e88a4d192898a2b137778bb46a1
Joel Gainey <jwgainey@hotmail.com>
2020-01-08 20:00:30 -0500

    added 1st lvl, mod, and took out redundancy

--- spells.js
+++ spells.js
@@ -2,85 +2,80 @@ var d4 = Math.floor(Math.random()*4) + 1;
 var d6 = Math.floor(Math.random()*6) + 1;
 var d8 = Math.floor(Math.random()*8) + 1;
 var d10 = Math.floor(Math.random()*10) + 1;
+var d12 = Math.floor(Math.random()*12) + 1;
+var dmg = 0;
+
 var spellCast = prompt("What spell do you cast?");
 var lvl = prompt("At what level?");
 
 if ( spellCast === 'absorb elements' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (1+(lvl-1)); i += 1) {
-         die = d6;
-         var abEl = dmg += die;  
+         var abEl = dmg += d6;  
    }
    document.write("You did " + abEl + " points of elemental damage!");
 }
 
 if ( spellCast === 'arms of hadar' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (2+(lvl-1)); i += 1) {
-         die = d6;
-         var armOfHad = dmg += die;  
+         var armOfHad = dmg += d6;  
    }
    document.write("You did " + armOfHad + " points of necrotic damage!");
 }
 
 if ( spellCast === 'burning hands' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (3+(lvl-1)); i += 1) {
-         die = d6;
-         var burHan = dmg += die;  
+         var burHan = dmg += d6;  
    }
    document.write("You did " + burHan + " points of fire damage!");
 }
 
 if ( spellCast === 'catapult' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (3+(lvl-1)); i += 1) {
-         die = d8;
-         var cat = dmg += die;  
+         var cat = dmg += d8;  
    }
    document.write("You did " + cat + " points of yeet damage!");
 }
 
 if ( spellCast === 'chaos bolt' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (1+(lvl-1)); i += 1) {
-         die = d6;
-         var cB = dmg += die;  
+         var cB = dmg += d6;  
    }
    document.write("You did " + ((2*d8) + cB) + " points of ??? damage!");
 } 
 
 if ( spellCast === 'chromatic orb' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (3+(lvl-1)); i += 1) {
-         die = d8;
-         var crOr = dmg += die;  
+         var crOr = dmg += d8;  
    }
    document.write("You did " + crOr + " points of elemental damage!");
 }
 
 if ( spellCast === 'color spray' ) {
-   var die, dmg = 0;
       for (var i = 0; i < 6+(2*(lvl-1)); i += 1) {
-         die = d10;
-         var colSp = dmg += die;  
+         var colSp = dmg += d10;  
    }
    document.write("You did " + colSp + " points of gaudy damage!");
 }
 
-// need to add modifier
 if ( spellCast === 'cure wounds' ) {
- //  var mod = prompt("What is your WIS modifier?");
-   var die, dmg = 0;
+     var mod = parseInt(prompt("What is your WIS modifier?"));
+         dmg = mod;
       for (var i = 0; i < (1+(lvl-1)); i += 1) {
-         die = d8;
-         var cuWo =  dam += die;  
+         var cuWo =  dmg += d10;  
    }
    document.write("You healed " + cuWo + " points of damage!");
 }
 
+if ( spellCast === 'earth tremor' ) {
+      for (var i = 0; i < (1+(lvl-1)); i += 1) {
+         var eT = dmg += d6;  
+   }
+   document.write("You did " + eT + " point of bludgeoning damage!");
+}
+
+//weird equation needed
 if ( spellCast === 'false life' ) {
-   var die, dmg = 4*lvl;
+   var die, dmg = 4;
       for (var i = 0; i < (1+(lvl-1)); i += 1) {
          die = d4;
          var fLf = dmg += die;  
@@ -89,139 +84,152 @@ if ( spellCast === 'false life' ) {
 }
 
 if ( spellCast === 'guiding bolt' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (4+(lvl-1)); i += 1) {
-         die = d6;
-         var guBo = dmg += die;  
+         var guBo = dmg += d6;  
    }
    document.write("You did " + guBo + " points of radiant damage!");
 }
 
 if ( spellCast === 'hail of thorns' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (1+(lvl-1)); i += 1) {
-         die = d10;
-         var haOfTh = dmg += die;  
+         var haOfTh = dmg += d10;  
    }
    document.write("You did " + haOfTh + " points of piercing damage!");
 }
 
 //modifier problem
 if ( spellCast === 'healing word' ) {
-   var die, dmg = 0;
+   var mod = parseInt(prompt("What is your WIS modifier?"));
+       dmg = mod;
       for (var i = 0; i < (1+(lvl-1)); i += 1) {
-         die = d4;
-         var heWo = dmg += die;  
+         var heWo = dmg += d4;  
    }
    document.write("You healed " + heWo + " points of damage!");
 }
 
 if ( spellCast === 'hellish rebuke' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (2+(lvl-1)); i += 1) {
-         die = d10;
-         var helReb = dmg += die;  
+         var helReb = dmg += d10;  
    }
    document.write("You did " + helReb + " points of fire damage!");
 }
 
 if ( spellCast === 'hex' ) {
-   var hex= d6;
-   document.write("Add "+ hex + " points of necrotic to your damage!");
+   document.write("Add "+ d6 + " points of necrotic to your damage!");
 }
 
 if ( spellCast === 'hunters mark' ) {
-   var hehMx= d6;
-   document.write("Add "+ hM + " points to your damage!");
+   document.write("Add "+ d6 + " points to your damage!");
 }
 
 if ( spellCast === 'ice knife' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (2+(lvl-1)); i += 1) {
-         die = d6;
-         var icKn2 = dmg += die;  
+         var icKn = dmg += d6;  
    }
-   document.write("You did " + d10 + " piercing damage and " + icKn2 +" points of cold damage!");
+   document.write("You did " + d10 + " piercing damage and " + icKn +" points of cold damage!");
 }
 
 if ( spellCast === 'inflict wounds' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (3+(lvl-1)); i += 1) {
-         die = d10;
-         var inWo = dmg += die;  
+         var inWo = dmg += d10;  
    }
    document.write("You did " + inWo + " points of necrotic damage!");
 }
 
 if ( spellCast === 'inflict wounds' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (3+(lvl-1)); i += 1) {
-         die = d10;
-         var inWo = dmg += die;  
+         var inWo = dmg += d10;  
    }
    document.write("You did " + inWo + " points of necrotic damage!");
 }
 
 if ( spellCast === 'magic missile' ) {
-   var die, dmg = 0;
       for (var i = 0; i < 3; i += 1) {
-         die = (d4 +1)* lvl;
-         var magMiss = dmg += die;  
+         dart = (d4 +1)* lvl;
+         var magMiss = dmg += dart;  
    }
    document.write("You did " + magMiss + " points of force damage!");
 }
 
+if ( spellCast === 'ray of sickness' ) {
+      for (var i = 0; i < (2+(lvl-1)); i += 1) {
+         var raOfSi = dmg += d8;  
+   }
+   document.write("You did " + raOfSi + " points of poison damage! Gross!");
+}
+
 if ( spellCast === 'sleep' ) {
-   var die, dmg = 0;
       for (var i = 0; i < 5+(2*(lvl-1)); i += 1) {
-         die = d8;
-         var slp = dmg += die;  
+         var slp = dmg += d8;  
    }
    document.write("You did " + slp + " points of sleep damage?");
 }
- 
-   
+
+if ( spellCast === 'searing smite' ) {
+      for (var i = 0; i < (1+(lvl-1)); i += 1) {
+         var seSm = dmg += d6;  
+   }
+   document.write("You did " + seSm + " points of fire damage!");
+}
+
+if ( spellCast === 'thunderous smite' ) { 
+   document.write("You did " + 2*d6 + " points of thunder damage!");
+}
+
+if ( spellCast === 'thunderwave' ) {
+      for (var i = 0; i < (2+(lvl-1)); i += 1) {
+         var thWa = dmg += d8;  
+   }
+   document.write("You did " + thWa + " points of thunder damage!");
+}
+
+if ( spellCast === 'witch bolt' ) {
+      for (var i = 0; i < (1+(lvl-1)); i += 1) {
+         var wiBo = dmg += d12;  
+   }
+   document.write("You did " + wiBo + " points of lightning damage! Pick a better spell!");
+}
+
+if ( spellCast === 'wrathful smite' ) {
+   document.write("You did " + d6 + " points of psychic damage!");
+}
+
+if ( spellCast === 'zephyr strike' ) {
+   document.write("You did " + d8 + " force damage!");
+}
+
 if ( spellCast === 'fireball' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (8+(lvl-3)); i += 1) {
-         die = d6;
-         var firBal = dmg += die;  
+         var firBal = dmg += d6;  
    }
    document.write("You did " + firBal + " points of fire damage!");
 }
 
 if ( spellCast === 'lightning bolt' ) {
-   var die, dmg = 0;
       for (var i = 0; i < (8+(lvl-3)); i += 1) {
-         die = d6;
-         var ligBlt = dmg += die;  
+         var ligBlt = dmg += d6;  
    }   
    document.write("You did " + ligBlt + " points of electric damage!");
 }
 
 if ( spellCast === 'finger of death' ) {
-   var die, dmg = 0;
       for (var i = 0; i < 7; i += 1) {
-         die = d8;
-         var finOfDea = dmg += die;
+         var finOfDea = dmg += d8;
    }
    document.write("You did " + finOfDea + " points of necrotic damage! Enjoy your zombie!");
 }
 
 if ( spellCast === 'disintegrate' ) {
-   var die, dmg = 40;
+    dmg = 40;
       for (var i = 0; i < (10+(3*(lvl-6))); i += 1) {
-         die = d6;
-         var disint = dmg += die;
+         var disint = dmg += d6;
    } 
    document.write("You did " + disint + " points of force damage! Ouch!"); 
 }  
 
 if ( spellCast === 'psychic scream' ) {
-   var die, dmg = 0;
       for (var i = 0; i < 14; i += 1) {
-         die = d6;
-         var psyScm = dmg += die;
+         var psyScm = dmg += d6;
    }  
    document.write("You did " + psyScm + " points of psychic damage! Watch for splatter!");
 }   
